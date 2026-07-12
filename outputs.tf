0ea863a50d4f7e97ec2ad8b2880149b07d43ee01
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "virtual_desktop_applications" {
-  description = "All virtual_desktop_application resources"
-  value       = azurerm_virtual_desktop_application.virtual_desktop_applications
-}
 output "virtual_desktop_applications_application_group_id" {
-  description = "List of application_group_id values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.application_group_id]
+  description = "Map of application_group_id values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.application_group_id }
 }
 output "virtual_desktop_applications_command_line_argument_policy" {
-  description = "List of command_line_argument_policy values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.command_line_argument_policy]
+  description = "Map of command_line_argument_policy values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.command_line_argument_policy }
 }
 output "virtual_desktop_applications_command_line_arguments" {
-  description = "List of command_line_arguments values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.command_line_arguments]
+  description = "Map of command_line_arguments values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.command_line_arguments }
 }
 output "virtual_desktop_applications_description" {
-  description = "List of description values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.description]
+  description = "Map of description values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.description }
 }
 output "virtual_desktop_applications_friendly_name" {
-  description = "List of friendly_name values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.friendly_name]
+  description = "Map of friendly_name values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.friendly_name }
 }
 output "virtual_desktop_applications_icon_index" {
-  description = "List of icon_index values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.icon_index]
+  description = "Map of icon_index values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.icon_index }
 }
 output "virtual_desktop_applications_icon_path" {
-  description = "List of icon_path values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.icon_path]
+  description = "Map of icon_path values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.icon_path }
 }
 output "virtual_desktop_applications_name" {
-  description = "List of name values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.name]
+  description = "Map of name values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.name }
 }
 output "virtual_desktop_applications_path" {
-  description = "List of path values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.path]
+  description = "Map of path values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.path }
 }
 output "virtual_desktop_applications_show_in_portal" {
-  description = "List of show_in_portal values across all virtual_desktop_applications"
-  value       = [for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : v.show_in_portal]
+  description = "Map of show_in_portal values across all virtual_desktop_applications, keyed the same as var.virtual_desktop_applications"
+  value       = { for k, v in azurerm_virtual_desktop_application.virtual_desktop_applications : k => v.show_in_portal }
 }
 
